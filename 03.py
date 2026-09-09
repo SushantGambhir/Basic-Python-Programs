45a929c745d46642478d8b4148c62e83d1871de6
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-09 21:27:53 +0530

    Python Program

---
+++ 03.py
@@ -0,0 +1,8 @@
+#  Write a Python Program to Check Leap Year. 
+
+year = int(input('Enter the year: '))
+
+if ((year%4==0 and year%100!=0) or (year%400==0)):
+    print('Leap Year')
+else:
+    print('Not leap year')
