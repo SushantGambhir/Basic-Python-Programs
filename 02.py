358770f03269b30843ff86937eec6ee3165022fb
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-09 21:19:43 +0530

    Python Program

--- 02.py
+++ 02.py
@@ -9,8 +9,8 @@ c = int(input('Enter the constant term: '))
 disc = b*b - 4*a*c
 
 if disc>0:
-    r1 = (-b + math.sqrt(disc))/2
-    r2 = (-b - math.sqrt(disc))/2
+    r1 = (-b + math.sqrt(disc))/(2*a)
+    r2 = (-b - math.sqrt(disc))/(2*a)
     print(f'The roots are: {r1} and {r2}')
 
 elif disc==0:
