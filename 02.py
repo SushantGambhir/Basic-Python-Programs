54c4e06821c683b5483ed081babc0ef47bb2f7f3
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-09 21:17:02 +0530

    Python Program

---
+++ 02.py
@@ -0,0 +1,21 @@
+#  Write a Python program to solve a quadratic equation. 
+
+import math
+
+a = int(input('Enter coefficient of x^2: '))
+b = int(input('Enter coefficient of x: '))
+c = int(input('Enter the constant term: '))
+
+disc = b*b - 4*a*c
+
+if disc>0:
+    r1 = (-b + math.sqrt(disc))/2
+    r2 = (-b - math.sqrt(disc))/2
+    print(f'The roots are: {r1} and {r2}')
+
+elif disc==0:
+    r = (-b + math.sqrt(disc))/2
+    print(f'Single root: {r}')
+
+else:
+    print('No real roots')
