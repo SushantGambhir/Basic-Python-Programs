68c36d395722d1720991372d9ba565ac2caa3a0b
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-10 22:20:38 +0530

    Python

--- 04.py
+++ 04.py
@@ -4,12 +4,15 @@ import math
 
 n = int(input('Enter a number: '))
 
+isPrime = True
+
 if n>1:
     for i in range(2,int(math.sqrt(n))+1):
         if n%i==0:
-            print('Number is not prime.')
+            isPrime=False
             break
-    else:
-        print('Number is prime.')
+
+if(isPrime):
+    print('Prime')
 else:
-    print('Number is not prime.')
+    print('Non Prime')
