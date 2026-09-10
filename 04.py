1f8e7099bfd5191e9ace056a03507c1c927fc518
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-10 22:19:13 +0530

    Python

--- 04.py
+++ 04.py
@@ -9,7 +9,7 @@ if n>1:
         if n%i==0:
             print('Number is not prime.')
             break
-        else:
-            print('Number is prime.')
+    else:
+        print('Number is prime.')
 else:
     print('Number is not prime.')
