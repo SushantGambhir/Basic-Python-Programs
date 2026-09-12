9418b2590de4001be753f25abbf028ac8c588b4c
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-09-12 20:13:12 +0530

    Python

--- 08.py
+++ 08.py
@@ -1,16 +1,19 @@
 #  Write a Python Program to Check Armstrong Number. 
 
+def checkArmstrong(num):
+    order = len(str(num))
+    sum = 0
+
+    temp = num
+    while temp > 0:
+        digit = temp%10
+        sum+=digit**order
+        temp//=10
+
+    if sum==num:
+        print('Number is Armstrong')
+    else:
+        print('Not Armstrong')
+
 num = int(input('Enter a number: '))
-order = len(str(num))
-sum = 0
-
-temp = num
-while temp > 0:
-    digit = temp%10
-    sum+=digit**order
-    temp//=10
-
-if sum==num:
-    print('Number is Armstrong')
-else:
-    print('Not Armstrong')
+checkArmstrong(num)
